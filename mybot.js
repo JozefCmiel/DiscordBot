c31fca46d9510637341a271bc9eb493d3d81bc4c
Nataron <39346144+Nataron@users.noreply.github.com>
2018-06-05 11:17:32 +0200

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -1,137 +1,15 @@
-// Load up the discord.js library
-const Discord = require("discord.js");
-
-// This is your client. Some people call it `bot`, some people call it `self`, 
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// Here we load the config.json file that contains our token and our prefix values. 
-
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
-
-client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-  
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-
-
-
-
-
-client.on("message", async message => {
-
-  
- 
-  if(message.author.bot) return;
-  
-
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  
- 
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
-  
-  
-  if(command === "pijymy?") {
-    var myArray = ['Jasne', 'Ni', 'Perhaps', 'Wypijymy!' , 'Rozhnodnie ni!'];
-    var rand = myArray[Math.floor(Math.random() * myArray.length)];
-    message.channel.send(`${rand} @everyone`);
-  }
-  
-  if(command === "flip") {
-    
-    var myArray = ['True', 'False'];
-    var rand = myArray[Math.floor(Math.random() * myArray.length)];
-   
-    message.channel.send(`${rand} ${message.author}`);
-  }
-  if(command === "roll"){
-      var min = parseInt(args[0]);
-      var max = parseInt(args[1]);
-      
-      if(min > max){
-          var tmp = max;
-          max = min;
-          min= tmp;
-          
-      }
-      var num = Math.floor(Math.random() *(max - min + 1))+min;
-    
-   message.channel.send(`${num} ${message.author}`);
-  }
-
-  
-  if(command === "kick") {
-    // This command must be limited to mods and admins. In this example we just hardcode the role names.
-    // Please read on Array.some() to understand this bit: 
-    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    // Let's first check if we have a member and if we can kick them!
-    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-    // We can also support getting the member by ID, which would be args[0]
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    
-    // slice(1) removes the first part, which here should be the user mention or ID
-    // join(' ') takes all the various parts to make it a single string.
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    // Now, time for a swift kick in the nuts!
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-
-  }
-  
-  if(command === "ban") {
-    // Most of this command is identical to kick, except that here we'll only let admins do it.
-    // In the real world mods could ban too, but this is just an example, right? ;)
-    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.bannable) 
-      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
-
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    await member.ban(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-  }
-  
-  if(command === "purge") {
-    // This command removes all messages from all users in the channel, up to 100.
-    
-    // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 10);
-    
-    // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({count: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-  }
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-client.login("NDUxNDI1MzMwNzUwODE2MjU2.DfFrcA.nkR6Ed9hXs2s4w6iMbBG2_b07DE");
-
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
