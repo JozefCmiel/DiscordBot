23a928212e4b27b105d18931cf06271b5afd0e95
Nataron <39346144+Nataron@users.noreply.github.com>
2018-06-05 11:40:28 +0200

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -1,15 +1,63 @@
+
 const Discord = require('discord.js');
+
+
 const client = new Discord.Client();
+
+
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
 
+
+
+
+
+client.on('message',  message => {
+
+  
+ 
+  if(message.author.bot) return;
+  
+
+  if(message.content.indexOf('!') !== 0) return;
+  
+ 
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  
+  
+  if(command === "pijymy?") {
+    var myArray = ['Jasne', 'Ni', 'Perhaps', 'Wypijymy!' , 'Rozhnodnie ni!'];
+    var rand = myArray[Math.floor(Math.random() * myArray.length)];
+    message.channel.send(`${rand} @everyone`);
+  }
+  
+  if(command === "flip") {
+    
+    var myArray = ['True', 'False'];
+    var rand = myArray[Math.floor(Math.random() * myArray.length)];
+   
+    message.channel.send(`${rand} ${message.author}`);
+  }
+  if(command === "roll"){
+      var min = parseInt(args[0]);
+      var max = parseInt(args[1]);
+      
+      if(min > max){
+          var tmp = max;
+          max = min;
+          min= tmp;
+          
+      }
+      var num = Math.floor(Math.random() *(max - min + 1))+min;
+    
+   message.channel.send(`${num} ${message.author}`);
+  }  
+ 
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
