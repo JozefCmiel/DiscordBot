2050d9d7207604446642ad0bde702e3cbb5ae05a
Nataron <39346144+Nataron@users.noreply.github.com>
2018-10-28 18:30:47 +0100

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -74,6 +74,14 @@ client.on('message',  message => {
     }
    
   }
+ if(command === "help"){
+     
+     message.channel.send(`!pick a b c - Vybere mezi a,b,c a vráti tuto hodnotu. 
+!roll - Vybere číslo mezi 0-10 a vráti ho .
+!roll a b - Vybere číslo mezi a-b a vráti ho .
+!flip - Vráti hodnotu True/False .
+!pijymy - Vráti hodnotu která je uložena v poli.`);
+ }
  
 });
 
